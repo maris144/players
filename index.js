00a5370f8011608ef922b15f87f2413582a5c15b
MARISOL CABRERA <marisolcabreracarrasco@gmail.com>
2020-02-24 17:51:04 -0500

    Agregar vista de creacion de jugador

--- index.js
+++ index.js
@@ -6,9 +6,33 @@ const axios = require("axios");
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.get("/create", function(req, res) {
+  res.render("create");
+});
+
+app.post("/create", function(req, res) {
+  let id = Number( req.body.id);
+  let first_name = req.body.first_name;
+  let last_name = req.body.last_name;
+  let height_feet = req.body.height_feet;
+  let position = req.body.position;
+  let newPlayer = {id:id,first_name:first_name,last_name:last_name,height_feet:height_feet,position:position};
+
+  let database = require("./database/players.json");
+  let players = [newPlayer,...database];
+  var jsonPath = path.join(__dirname, "database", "players.json");
+  fs.writeFileSync(jsonPath, JSON.stringify(players));
+  res.redirect("/home");
+});
+
 app.get("/view", function(req, res) {
   let id = req.query.id;
-  let database = require("./database/players.json");
+  var jsonPath = path.join(__dirname, "database", "players.json");
+  var contents = fs.readFileSync(jsonPath);
+  let database = JSON.parse(contents);
   let player = database.find(p => p.id == id);
   res.render("view", { player: player });
 });
