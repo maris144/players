3ad425255b70f93dd779117cd7a35d8731252cb0
MARISOL CABRERA <marisolcabreracarrasco@gmail.com>
2020-02-25 12:13:50 -0500

    Finalizado

---
+++ README.md
@@ -0,0 +1,4 @@
+### Instrucciones
+1. Instalar node js
+2. En una terminar ubicada en el directorio de trabajo correr el comando node index.js
+3. Abrir en un navegador la ruta localhost:3000
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const path = require("path");
 const axios = require("axios");
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded());
@@ -13,17 +15,18 @@ app.post("/filter", function(req, res) {
   let id = req.body.id;
   let first_name = req.body.first_name;
   let last_name = req.body.last_name;
-  let query = (id?"id="+id:"")+(first_name?"first_name="+first_name:"")+(last_name?"last_name="+last_name:"");
-  query = query.length>0?"?"+query:query;
-  res.redirect("/home"+query);
+  let query =
+    (id ? "id=" + id : "") +
+    (first_name ? "first_name=" + first_name : "") +
+    (last_name ? "last_name=" + last_name : "");
+  query = query.length > 0 ? "?" + query : query;
+  res.redirect("/home" + query);
 });
-
-
 
 app.get("/edit", function(req, res) {
   let id = req.query.id;
-  var jsonPath = path.join(__dirname, "database", "players.json");
-  var contents = fs.readFileSync(jsonPath);
+  let jsonPath = path.join(__dirname, "database", "players.json");
+  let contents = fs.readFileSync(jsonPath);
   let database = JSON.parse(contents);
   let player = database.find(p => p.id == id);
   res.render("edit", { player: player });
@@ -36,8 +39,8 @@ app.post("/edit", function(req, res) {
   let height_feet = req.body.height_feet;
   let position = req.body.position;
 
-  var jsonPath = path.join(__dirname, "database", "players.json");
-  var contents = fs.readFileSync(jsonPath);
+  let jsonPath = path.join(__dirname, "database", "players.json");
+  let contents = fs.readFileSync(jsonPath);
   let database = JSON.parse(contents);
   let player = database.find(p => p.id == id);
   player.first_name = first_name;
@@ -69,15 +72,15 @@ app.post("/create", function(req, res) {
 
   let database = require("./database/players.json");
   let players = [newPlayer, ...database];
-  var jsonPath = path.join(__dirname, "database", "players.json");
+  let jsonPath = path.join(__dirname, "database", "players.json");
   fs.writeFileSync(jsonPath, JSON.stringify(players));
   res.redirect("/home");
 });
 
 app.get("/view", function(req, res) {
   let id = req.query.id;
-  var jsonPath = path.join(__dirname, "database", "players.json");
-  var contents = fs.readFileSync(jsonPath);
+  let jsonPath = path.join(__dirname, "database", "players.json");
+  let contents = fs.readFileSync(jsonPath);
   let database = JSON.parse(contents);
   let player = database.find(p => p.id == id);
   res.render("view", { player: player });
@@ -87,7 +90,7 @@ app.get("/delete", function(req, res) {
   let id = req.query.id;
   let database = require("./database/players.json");
   let players = database.filter(p => p.id != id);
-  var jsonPath = path.join(__dirname, "database", "players.json");
+  let jsonPath = path.join(__dirname, "database", "players.json");
   fs.writeFileSync(jsonPath, JSON.stringify(players));
   res.redirect("/home");
 });
@@ -97,22 +100,22 @@ app.get("/home", function(req, res) {
   let first_name = req.query.first_name;
   let last_name = req.query.last_name;
 
-  var jsonPath = path.join(__dirname, "database", "players.json");
-  var contents = fs.readFileSync(jsonPath);
+  let jsonPath = path.join(__dirname, "database", "players.json");
+  let contents = fs.readFileSync(jsonPath);
 
   let database = JSON.parse(contents);
   database = database.filter(
     p =>
       p.id == (id ? id : p.id) &&
-      p.first_name.startsWith (first_name ? first_name : p.first_name) &&
-      p.last_name.startsWith (last_name ? last_name : p.last_name)
+      p.first_name.startsWith(first_name ? first_name : p.first_name) &&
+      p.last_name.startsWith(last_name ? last_name : p.last_name)
   );
-  
+
   res.render("home", { database: database });
 });
 
 app.get("/", function(req, res) {
-  var jsonPath = path.join(__dirname, "database", "players.json");
+  let jsonPath = path.join(__dirname, "database", "players.json");
 
   let players = [];
   axios.get("https://www.balldontlie.io/api/v1/players").then(response => {
